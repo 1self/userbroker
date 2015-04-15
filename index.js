f9375b611da141e7f49465e65c39ea8c480087c8
Ed Sykes <ed.sykes@gmail.com>
2015-04-15 16:16:52 +0100

    added ability to send to devflow

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ winston.warn("Warns will be logged here");
 winston.info("Info will be logged here");
 winston.debug("Debug will be logged here");
 
+process.on('uncaughtException', function(err) {
+  winston.error('Caught exception: ' + err);
+});
+
 processor.setLogger(winston);
 
 var redisSubscribe = redis.createClient();
@@ -34,20 +38,23 @@ MongoClient.connect(url, function(err, db) {
 
 	
 	redisSubscribe.on('message', function(channel, message){
-		winston.info("message recieved from channel " + channel);
+		winston.info('change');
+		winston.info('message recieved from channel ' + channel);
+		winston.info('dddddddd');
 
 		if(channel === 'events'){
-			winston.debug('event message seen');
+			winston.info('doing events');
+			winston.info('event message seen', message);
 			var event = JSON.parse(message);	
-			processor.processStreamEvent(event, users)
+			processor.processStreamEvent(event, users);
 		}
 		else if(channel === 'users'){
 			winston.debug('passing message to user processor');
 			var userMessage = JSON.parse(message);
-			processor.processUserEvent(userMessage);
+			processor.processUserEvent(userMessage, users);
 		}
 		else{
-			winston.error('unknown event type');
+			winston.info('unknown event type');
 		}
 
 	});
--- processor.js
+++ processor.js
@@ -1,21 +1,72 @@
 'use strict';
+var _ = require('lodash');
+var request = require('request');
+var logger = require('winston');
 
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 
-//var _ = require('lodash');
-var logger = require('winston');
-
 var setLogger = function (newLogger){
 	logger = newLogger;
 };
 
+var users = {};
+var streamsToUsers = {};
+
 var processStreamEvent = function(streamEvent){
 	logger.info('processed an event', streamEvent);
+	var streamid = streamEvent.streamid;
+	logger.info('looking up user for streamid', streamid);
+	var user = streamsToUsers[streamid];
+	if(user === undefined){
+		logger.info('no user found');
+		return;
+	}
+
+	streamEvent.user = user.username;
+	logger.info('bbb');
+	logger.info('making request to flow app');
+	
+
+	var streamEvents = [];
+	streamEvents.push(streamEvent);
+	var options = {
+	  method: 'post',
+	  body: streamEvents,
+	  json: true,
+	  url: 'http://devflow.azurewebsites.net/api/events'
+	}
+
+	request.post(options, function(error, response){
+		logger.info('message brokered', response);
+	});
+	logger.info('processed event for user', user.username);
+
 };
 
-var processUserEvent = function(userEvent){
+// eas: on any user event we reload the whole user
+var processUserEvent = function(userEvent, userRepository){
+	logger.info('loading user into cache', userEvent.username);
+	var condition = {
+		username: userEvent.username
+	};
+	userRepository.findOne(condition, function(error, user){
+		if(error){
+			logger.error('error while retrieving user', error);
+			return;
+		}
+
+		logger.debug('loaded user from database:', user);
+
+		users[user.username] = user;
+		_.map(user.streams, function(stream){
+			streamsToUsers[stream.streamid] = user;
+			logger.debug('mapped ' + stream.streamid + ' to ' + user.username);
+		});
+
+	});
+	
 	logger.info('processed a user event', userEvent);
 };
 
